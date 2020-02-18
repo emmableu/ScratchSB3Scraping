6abfc6ab68715dad8befc7b5cfbfb0bd190550ed
wwang33 <wwang33@ncsu.edu>
2020-02-17 21:57:39 -0500

    ready for remote

--- sb3_scrape.py
+++ sb3_scrape.py
@@ -2,23 +2,39 @@ import urllib.request
 import urllib.error
 import json
 
-project_ids = []
-page = 0
 
-try:
-    while page + 40 <= 2000:
-        url = "https://api.scratch.mit.edu/explore/projects?limit=40&offset=" \
-            + str(page) \
-            + "&language=en&mode=trending&q=animations"
-        page += 40
-        req = urllib.request.Request(url, headers={'User-Agent': "Magic Browser"})
-        res = urllib.request.urlopen(req)
-        data = json.load(res)
-        print("Page: "+str(page))
-        for prj in data:
-            prj_id = prj['id']
-            project_ids.append(prj_id)
-            download_url = "https://projects.scratch.mit.edu/" + str(prj_id)
-            urllib.request.urlretrieve(download_url, 'files/' + str(prj_id) + '.json')
-except urllib.error.HTTPError as e:
-    print(e.fp.read())
+
+import sys
+
+
+
+def main():
+    # print command line arguments
+    # genre = sys.argv[1]
+    genres = ['animations', 'games']
+    project_ids = []
+    page = 0
+    for genre in genres:
+
+        for i in range(0, 40):
+            try:
+                while page + 40 <= 200*(i+1):
+                    url = "https://api.scratch.mit.edu/explore/projects?limit=40&offset=" \
+                        + str(page) \
+                        + "&language=en&mode=trending&q=" + genre
+                    page += 40
+                    req = urllib.request.Request(url, headers={'User-Agent': "Magic Browser"})
+                    res = urllib.request.urlopen(req)
+                    data = json.load(res)
+                    print("Page: "+str(page))
+                    for prj in data:
+                        prj_id = prj['id']
+                        project_ids.append(prj_id)
+                        download_url = "https://projects.scratch.mit.edu/" + str(prj_id)
+                        urllib.request.urlretrieve(download_url, genre + '/' + str(prj_id) + '.json')
+            except urllib.error.HTTPError as e:
+                print(e.fp.read())
+
+
+if __name__ == "__main__":
+    main()
